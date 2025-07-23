b1ce34ba685f2c64b7e16cc1718521ac64f881bf
josemodolo <josenetozxc@yahoo.com>
2025-07-23 11:07:51 -0300

    first commit design pattern study

---
+++ src/main/java/org/example/one/digitalinnovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,13 @@
+package org.example.one.digitalinnovation.gof;
+
+public class SingletonLazyHolder {
+    private static class InstanceHolder{
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+    private SingletonLazyHolder(){
+        super();
+    }
+    public static SingletonLazyHolder getInstancia(){
+        return InstanceHolder.instancia;
+    }
+}
---
+++ src/main/java/org/example/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,22 @@
+package org.example.one.digitalinnovation.gof;
+
+public class Test {
+    public static void main (String[] args){
+        //Tests related to the Singleton Design Pattern:
+
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+    }
+}
